1d9939cef50c1c54583a1df41a5406159982ab63
Alexander Kolotov <alexandr.kolotov@gmail.com>
2019-08-23 00:22:32 +0300

    specify sender rather than bridge contracts, proper sending of erc20 tokens

--- src/test-services/ethereumSend/testEthereumSend.js
+++ src/test-services/ethereumSend/testEthereumSend.js
@@ -1,6 +1,7 @@
 const Web3 = require('web3')
+const BN = require('bignumber.js')
 
-const { HOME_RPC_URL, HOME_BRIDGE_ADDRESS, HOME_CHAIN_ID, HOME_PRIVATE_KEY, HOME_TOKEN_ADDRESS } = process.env
+const { HOME_RPC_URL, RECEIVER_ADDRESS, HOME_CHAIN_ID, HOME_PRIVATE_KEY, HOME_TOKEN_ADDRESS } = process.env
 
 const abiToken = require('./IERC20').abi
 
@@ -9,18 +10,18 @@ const token = new web3.eth.Contract(abiToken, HOME_TOKEN_ADDRESS)
 
 const amount = parseInt(process.argv[2])
 
-const deployAddress = web3.eth.accounts.privateKeyToAccount(`0x${HOME_PRIVATE_KEY}`).address
+const sender = web3.eth.accounts.privateKeyToAccount(`0x${HOME_PRIVATE_KEY}`).address
 
 async function main () {
-  console.log(`Transfer from ${deployAddress} to ${HOME_BRIDGE_ADDRESS}, ${amount} tokens`)
+  console.log(`Transfer from ${sender} to ${RECEIVER_ADDRESS}, ${amount} tokens`)
 
-  const query = token.methods.transfer(HOME_BRIDGE_ADDRESS, amount)
+  const query = token.methods.transfer(RECEIVER_ADDRESS, '0x'+(new BN(amount).toString(16)))
   const encodedABI = query.encodeABI()
   const tx = {
     data: encodedABI,
-    from: deployAddress,
+    from: sender,
     to: HOME_TOKEN_ADDRESS,
-    nonce: await web3.eth.getTransactionCount(deployAddress),
+    nonce: await web3.eth.getTransactionCount(sender),
     chainId: parseInt(HOME_CHAIN_ID)
   }
   tx.gas = Math.min(Math.ceil(await query.estimateGas(tx) * 1.5), 6721975)
