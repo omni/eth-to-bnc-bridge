4c45436588ffac3acac993c636bb452b17417dc7
Kirill Fedoseev <k.fedoseev@innopolis.university>
2019-08-25 16:01:39 +0300

    Refactor encode to use generators

--- src/oracle/proxy/encode.js
+++ src/oracle/proxy/encode.js
@@ -6,141 +6,121 @@ function padZeros (s, len) {
   return s
 }
 
-function makeBuffer (value, length, base = 10) {
+function makeBuffer (value, length = 32, base = 16) {
   return Buffer.from(padZeros(new BN(value, base).toString(16), length * 2), 'hex')
 }
 
 const keygenEncoders = [
   null,
   // round 1
-  function (value) {
-    const buffers = []
-    buffers.push(makeBuffer(value.e.n, 256))
-    buffers.push(makeBuffer(value.com, 32, 16))
+  function * (value) {
+    yield makeBuffer(value.e.n, 256, 10)
+    yield makeBuffer(value.com)
     for (let x of value.correct_key_proof.sigma_vec) {
-      buffers.push(makeBuffer(x, 256))
+      yield makeBuffer(x, 256, 10)
     }
-    return Buffer.concat(buffers)
   },
   // round 2
-  function (value) {
-    const buffers = []
-    buffers.push(makeBuffer(value.blind_factor, 32, 16))
-    buffers.push(makeBuffer(value.y_i.x, 32, 16))
-    buffers.push(makeBuffer(value.y_i.y, 32, 16))
-    return Buffer.concat(buffers)
+  function * (value) {
+    yield makeBuffer(value.blind_factor)
+    yield makeBuffer(value.y_i.x)
+    yield makeBuffer(value.y_i.y)
   },
   // round 3
-  function (value) {
-    const buffers = []
-    buffers.push(Buffer.from(value.ciphertext)) // 32 bytes
-    buffers.push(Buffer.from(value.tag)) // 16 bytes
-    return Buffer.concat(buffers)
+  function * (value) {
+    yield Buffer.from(value.ciphertext) // 32 bytes
+    yield Buffer.from(value.tag) // 16 bytes
   },
   // round 4
-  function (value) {
-    const buffers = []
-    buffers.push(Buffer.from([ value.parameters.threshold ])) // 1 byte
-    buffers.push(Buffer.from([ value.parameters.share_count ])) // 1 byte
+  function * (value) {
+    yield Buffer.from([ value.parameters.threshold ]) // 1 byte
+    yield Buffer.from([ value.parameters.share_count ]) // 1 byte
     for (let x of value.commitments) {
-      buffers.push(makeBuffer(x.x, 32, 16))
-      buffers.push(makeBuffer(x.y, 32, 16))
+      yield makeBuffer(x.x)
+      yield makeBuffer(x.y)
     }
-    return Buffer.concat(buffers)
   },
   // round 5
-  function (value) {
-    const buffers = []
-    buffers.push(makeBuffer(value.pk.x, 32, 16))
-    buffers.push(makeBuffer(value.pk.y, 32, 16))
-    buffers.push(makeBuffer(value.pk_t_rand_commitment.x, 32, 16))
-    buffers.push(makeBuffer(value.pk_t_rand_commitment.y, 32, 16))
-    buffers.push(makeBuffer(value.challenge_response, 32, 16))
-    return Buffer.concat(buffers)
+  function * (value) {
+    yield makeBuffer(value.pk.x)
+    yield makeBuffer(value.pk.y)
+    yield makeBuffer(value.pk_t_rand_commitment.x)
+    yield makeBuffer(value.pk_t_rand_commitment.y)
+    yield makeBuffer(value.challenge_response)
   }
 ]
 
 const signEncoders = [
   // round 0
-  function (value) {
-    return Buffer.from([ value ])
+  function * (value) {
+    yield Buffer.from([ value ])
   },
   // round 1
-  function (value) {
-    const buffers = []
-    buffers.push(makeBuffer(value[0].com, 32, 16))
-    buffers.push(makeBuffer(value[1].c, 512, 16))
-    return Buffer.concat(buffers)
+  function * (value) {
+    yield makeBuffer(value[0].com)
+    yield makeBuffer(value[1].c, 512)
   },
   // round 2
-  function (value) {
-    const buffers = []
+  function * (value) {
     for (let i = 0; i < 2; i++) {
-      buffers.push(makeBuffer(value[i].c, 512, 16))
-      buffers.push(makeBuffer(value[i].b_proof.pk.x, 32, 16))
-      buffers.push(makeBuffer(value[i].b_proof.pk.y, 32, 16))
-      buffers.push(makeBuffer(value[i].b_proof.pk_t_rand_commitment.x, 32, 16))
-      buffers.push(makeBuffer(value[i].b_proof.pk_t_rand_commitment.y, 32, 16))
-      buffers.push(makeBuffer(value[i].b_proof.challenge_response, 32, 16))
-      buffers.push(makeBuffer(value[i].beta_tag_proof.pk.x, 32, 16))
-      buffers.push(makeBuffer(value[i].beta_tag_proof.pk.y, 32, 16))
-      buffers.push(makeBuffer(value[i].beta_tag_proof.pk_t_rand_commitment.x, 32, 16))
-      buffers.push(makeBuffer(value[i].beta_tag_proof.pk_t_rand_commitment.y, 32, 16))
-      buffers.push(makeBuffer(value[i].beta_tag_proof.challenge_response, 32, 16))
+      yield makeBuffer(value[i].c, 512)
+      yield makeBuffer(value[i].b_proof.pk.x)
+      yield makeBuffer(value[i].b_proof.pk.y)
+      yield makeBuffer(value[i].b_proof.pk_t_rand_commitment.x)
+      yield makeBuffer(value[i].b_proof.pk_t_rand_commitment.y)
+      yield makeBuffer(value[i].b_proof.challenge_response)
+      yield makeBuffer(value[i].beta_tag_proof.pk.x)
+      yield makeBuffer(value[i].beta_tag_proof.pk.y)
+      yield makeBuffer(value[i].beta_tag_proof.pk_t_rand_commitment.x)
+      yield makeBuffer(value[i].beta_tag_proof.pk_t_rand_commitment.y)
+      yield makeBuffer(value[i].beta_tag_proof.challenge_response)
     }
-    return Buffer.concat(buffers)
   },
   // round 3
-  function (value) {
-    return makeBuffer(value, 32, 16)
+  function * (value) {
+    yield makeBuffer(value)
   },
   // round 4
-  function (value) {
-    const buffers = []
-    buffers.push(makeBuffer(value.blind_factor, 32, 16))
-    buffers.push(makeBuffer(value.g_gamma_i.x, 32, 16))
-    buffers.push(makeBuffer(value.g_gamma_i.y, 32, 16))
-    return Buffer.concat(buffers)
+  function * (value) {
+    yield makeBuffer(value.blind_factor)
+    yield makeBuffer(value.g_gamma_i.x)
+    yield makeBuffer(value.g_gamma_i.y)
   },
   // round 5
-  function (value) {
-    return makeBuffer(value.com, 32, 16)
+  function * (value) {
+    yield makeBuffer(value.com)
   },
   // round 6
-  function (value) {
-    const buffers = []
-    buffers.push(makeBuffer(value[0].V_i.x, 32, 16))
-    buffers.push(makeBuffer(value[0].V_i.y, 32, 16))
-    buffers.push(makeBuffer(value[0].A_i.x, 32, 16))
-    buffers.push(makeBuffer(value[0].A_i.y, 32, 16))
-    buffers.push(makeBuffer(value[0].B_i.x, 32, 16))
-    buffers.push(makeBuffer(value[0].B_i.y, 32, 16))
-    buffers.push(makeBuffer(value[0].blind_factor, 32, 16))
-    buffers.push(makeBuffer(value[1].T.x, 32, 16))
-    buffers.push(makeBuffer(value[1].T.y, 32, 16))
-    buffers.push(makeBuffer(value[1].A3.x, 32, 16))
-    buffers.push(makeBuffer(value[1].A3.y, 32, 16))
-    buffers.push(makeBuffer(value[1].z1, 32, 16))
-    buffers.push(makeBuffer(value[1].z2, 32, 16))
-    return Buffer.concat(buffers)
+  function * (value) {
+    yield makeBuffer(value[0].V_i.x)
+    yield makeBuffer(value[0].V_i.y)
+    yield makeBuffer(value[0].A_i.x)
+    yield makeBuffer(value[0].A_i.y)
+    yield makeBuffer(value[0].B_i.x)
+    yield makeBuffer(value[0].B_i.y)
+    yield makeBuffer(value[0].blind_factor)
+    yield makeBuffer(value[1].T.x)
+    yield makeBuffer(value[1].T.y)
+    yield makeBuffer(value[1].A3.x)
+    yield makeBuffer(value[1].A3.y)
+    yield makeBuffer(value[1].z1)
+    yield makeBuffer(value[1].z2)
   },
   // round 7
-  function (value) {
-    return makeBuffer(value.com, 32, 16)
+  function * (value) {
+    yield makeBuffer(value.com)
   },
   // round 8
-  function (value) {
-    const buffers = []
-    buffers.push(makeBuffer(value.u_i.x, 32, 16))
-    buffers.push(makeBuffer(value.u_i.y, 32, 16))
-    buffers.push(makeBuffer(value.t_i.x, 32, 16))
-    buffers.push(makeBuffer(value.t_i.y, 32, 16))
-    buffers.push(makeBuffer(value.blind_factor, 32, 16))
-    return Buffer.concat(buffers)
+  function * (value) {
+    yield makeBuffer(value.u_i.x)
+    yield makeBuffer(value.u_i.y)
+    yield makeBuffer(value.t_i.x)
+    yield makeBuffer(value.t_i.y)
+    yield makeBuffer(value.blind_factor)
   },
   // round 9
-  function (value) {
-    return makeBuffer(value, 32, 16)
+  function * (value) {
+    yield makeBuffer(value)
   },
 ]
 
@@ -148,9 +128,20 @@ module.exports = function (isKeygen, round, value) {
   const parsedValue = JSON.parse(value)
   const roundNumber = parseInt(round[round.length - 1])
   const encoder = (isKeygen ? keygenEncoders : signEncoders)[roundNumber]
-  const encoded = encoder(parsedValue)
+  const generator = encoder(parsedValue)
+  const buffers = []
+  let next
+  while (true) {
+    next = generator.next()
+    if (next.done)
+      break
+    buffers.push(next.value)
+  }
+  const encoded = Buffer.concat(buffers)
   console.log(`Raw data: ${value.length} bytes, encoded data: ${encoded.length} bytes`)
   return encoded
 }
 
+
+module.exports(true, 'round2', '{"blind_factor":"11223344556677889900", "y_i":{"x":"00112233445566778899", "y":"00112233445566778899"}}')
 
