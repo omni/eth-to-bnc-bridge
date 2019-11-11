57cec2835703158a6d3ed0cdf1257e5e92fff717
Kirill Fedoseev <k.fedoseev@innopolis.university>
2019-11-11 20:25:08 +0300

    Not failing docker kill/rm

--- tests/init.sh
+++ tests/init.sh
@@ -6,8 +6,8 @@ echo "Cleaning previous demo environment"
 ./demo/clean.sh
 
 echo "Killing all remaining validator docker containers"
-docker kill $(docker ps | grep validator[1-3]_ | awk '{print $1}') > /dev/null 2>&1
-docker rm $(docker ps | grep validator[1-3]_ | awk '{print $1}') > /dev/null 2>&1
+docker kill $(docker ps | grep validator[1-3]_ | awk '{print $1}') > /dev/null 2>&1 || true
+docker rm $(docker ps | grep validator[1-3]_ | awk '{print $1}') > /dev/null 2>&1 || true
 
 echo "Building tss clients"
 docker build -t tss ./src/tss
