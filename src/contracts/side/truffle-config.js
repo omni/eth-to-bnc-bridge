cf54d9fe7112106e6a8ecb3daaa4c7d7f2a2642f
Kirill Fedoseev <k.fedoseev@innopolis.university>
2019-12-29 12:08:41 +0700

    Added coverage config in truffle config

--- src/contracts/side/truffle-config.js
+++ src/contracts/side/truffle-config.js
@@ -4,6 +4,13 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: 55
+    },
+    coverage: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: 55,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
     }
   },
   compilers: {
